252d6fd05ab88170657d2cc45e6449e2f3d8415b
Eduard Gevorkyan <egevorky@redhat.com>
2026-01-04 23:56:22 +0100

    Fix wasm32 guest panic handler and CI engines matrix

--- guest-wasm/src/lib.rs
+++ guest-wasm/src/lib.rs
@@ -4,3 +4,9 @@
 #[cfg(target_arch = "wasm32")]
 #[no_mangle]
 pub extern "C" fn main() {}
+
+/// Abort-on-panic for no_std wasm builds.
+#[cfg_attr(not(test), panic_handler)]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
